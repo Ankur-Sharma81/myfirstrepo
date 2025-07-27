2f0595603a381bfe8b554acc348f274526702c9d
Ankur-Sharma81 <ankursharmamd@gmail.com>
2025-07-28 01:24:21 +0530

    Added linear serach program in java

--- Arrays.java
+++ Arrays.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 public class Arrays{
     public static void Update(int marks[]){
         for(int i =0; i<marks.length; i++){
@@ -14,3 +14,28 @@ public class Arrays{
         System.out.println();
     }
 }
+*/
+import java.util.Scanner;
+public class Main{
+    public static int linearsearch(int numbers[], int key){
+        for(int i = 0; i<numbers.length; i++){
+            if(numbers[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int numbers[] = {2,4,6,8,10,12,14,26};
+        int key = 14;
+        int index = linearsearch(numbers, key);
+        if(index == -1){
+            System.out.print("Not found");
+        }
+        else{
+            System.out.print("found at index: " + index);
+        }
+    }
+}
+
+
--- Main.java
+++ Main.java
@@ -485,17 +485,23 @@ class Main{
 }*/
 import java.util.Scanner;
 public class Main{
-    public static void Update(int marks[]){
-        for(int i =0; i<marks.length; i++){
-            marks[i] = marks[i] + 1;
+    public static int linearsearch(int numbers[], int key){
+        for(int i = 0; i<numbers.length; i++){
+            if(numbers[i] == key){
+                return i;
+            }
         }
+        return -1;
     }
     public static void main(String[] args){
-        int marks[] = {97, 98, 99};
-        Update(marks);
-        for(int i =0; i<marks.length; i++){
-            System.out.print(marks[i] + " ");
+        int numbers[] = {2,4,6,8,10,12,14,26};
+        int key = 14;
+        int index = linearsearch(numbers, key);
+        if(index == -1){
+            System.out.print("Not found");
         }
-        System.out.println();
+        else{
+            System.out.print("found at index: " + index);
+        }
     }
 }
